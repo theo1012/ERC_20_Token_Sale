693290401f45a951cc3d3b53702a8a9db0ab7ac4
theo1012 <theodoreferguson1012@outlook.com>
2022-10-28 09:02:20 -0700

    Delegate transfer with Approve, transferFrom implemented

--- test/DappToken.js
+++ test/DappToken.js
@@ -41,4 +41,49 @@ contract('DappToken', function(accounts) {
         balance = await this.tokenInstance.balanceOf(accounts[0]);
         assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
     });
+
+    it('approves tokens for delegated transfer', async () => {
+        const success = await this.tokenInstance.approve.call(accounts[1], 100);
+        assert.equal(success, true, 'it returns true');
+        const receipt = await this.tokenInstance.approve(accounts[1], 100, {from: accounts[0]});
+        assert.equal(receipt.logs.length, 1, 'triggers one event');
+        assert.equal(receipt.logs[0].event, 'Approval', 'should be the Approval event');
+        assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
+        assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the acocunt the tokens are authorized to');
+        assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+        const allowance = await this.tokenInstance.allowance(accounts[0], accounts[1]);
+        assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer');
+    });
+
+    it('handles delegated token transfers', async () => {
+        fromAccount = accounts[2];
+        toAccount = accounts[3];
+        spendingAccount = accounts[4];
+
+        let receipt = await this.tokenInstance.transfer(fromAccount, 100, {from: accounts[0]});
+        receipt = await this.tokenInstance.approve(spendingAccount, 10, {from: fromAccount});
+        
+        try {
+            await this.tokenInstance.transferFrom(fromAccount, toAccount, 99, {from: spendingAccount});
+        } catch (error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than balance');
+        }
+        try {
+            await this.tokenInstance.transferFrom(fromAccount, toAccount, 20, {from: spendingAccount});
+        } catch (error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than approved amount');
+        }
+        receipt = await this.tokenInstance.transferFrom(fromAccount, toAccount, 10, {from: spendingAccount});
+        assert.equal(receipt.logs.length,1, 'triggers one event');
+        assert.equal(receipt.logs[0].event, 'Transfer', 'should be the Transfer event');
+        assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred from');
+        assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
+        assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+        balance = await this.tokenInstance.balanceOf(fromAccount);
+        assert.equal(balance.toNumber(), 90, 'deducts the amount from the sending account');
+        balance = await this.tokenInstance.balanceOf(toAccount);
+        assert.equal(balance.toNumber(), 10, 'adds the amount to the receiving account');
+        allowance = await this.tokenInstance.allowance(fromAccount, spendingAccount);
+        assert.equal(allowance.toNumber(), 0, 'deducts the amount from the allowance');
+    });
 });
